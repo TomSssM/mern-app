c28bbec058c1998668941628184a2cb6fce6d964
TomSssM <ilyashome3@gmail.com>
2021-06-26 17:40:36 +0300

    add ErrorBoundary

--- src/client/components/App/App.jsx
+++ src/client/components/App/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ErrorBoundary from '../ErrorBoundary';
 import IntlProvider from '../IntlProvider';
 import Layout from '../Layout';
 import Footer from '../Footer';
 import { Home, NotFound, Category } from '../../pages';
 
 const App = () => (
-  <Router>
-    <IntlProvider>
+  <IntlProvider>
+    <Router>
       <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/category/:id" component={Category} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-        <Footer />
+        <ErrorBoundary>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/category/:id" component={Category} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+          <Footer />
+        </ErrorBoundary>
       </Layout>
-    </IntlProvider>
-  </Router>
+    </Router>
+  </IntlProvider>
 );
 
 export default App;
---
+++ src/client/components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ErrorPage from '../ErrorPage';
+
+// todo: redux state
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError() {
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidCatch(error) {
+    const { onError } = this.props;
+    onError?.(error);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return <ErrorPage />;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;

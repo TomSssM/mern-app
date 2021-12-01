ba1f7fe4ccf34f1446083c6ca96b9137e897caa1
TomSssM <ilyashome3@gmail.com>
2021-12-02 00:58:01 +0300

    add yet another todo

--- client/index.jsx
+++ client/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.scss';
 
-// todo: bring: IntlProvider, BrowserRouter:Router and ErrorBoundary to this file
+// todo: bring: IntlProvider, BrowserRouter:Router, ErrorBoundary and React.StrictMode to this file
 ReactDOM.render(<App />, document.getElementById('root'));
